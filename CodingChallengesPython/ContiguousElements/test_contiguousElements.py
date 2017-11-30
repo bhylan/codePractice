5fcc7482e5582b2b499afa00e81ab25c004e0e58
Hylan, BreaAnne <Breaanne_Hylan@intuit.com>
2017-11-30 08:49:23 -0800

    adding contiguouselements python challenge

---
+++ CodingChallengesPython/ContiguousElements/test_contiguousElements.py
@@ -0,0 +1,24 @@
+#
+# Given an array of distinct integers, find length of the longest subarray which contains numbers that can be arranged in a continuous sequence. 
+#  Input:  arr[] = {10, 12, 11};
+#  Output: Length of the longest contiguous subarray is 3
+#
+#  Input:  arr[] = {14, 12, 11, 20};
+#   Output: Length of the longest contiguous subarray is 2
+# 
+#   Input:  arr[] = {1, 56, 58, 57, 90, 92, 94, 93, 91, 45};
+#   Output: Length of the longest contiguous subarray is 5
+# 
+import unittest
+
+def findLength(input):
+    return 
+    
+def test_answer_1():
+    assert findLength([10,12,11]) == 3
+    
+def test_answer_2():
+    assert findLength([14, 12, 11, 20]) == 2
+    
+def test_answer_3():
+    assert findLength([1, 56, 58, 57, 90, 92, 94, 93, 91, 45]) == 5
